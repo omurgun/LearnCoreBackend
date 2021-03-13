96d1e0e6502858bc31d429b0f583e87ba108a89b
ömür <omur_gun@outlook.com>
2021-03-13 19:33:32 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/LearnCoreBackendContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class LearnCoreBackendContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=OMURGUNPC;Integrated Security=True;Database=LearnCoreBackend;Trusted_Connection=true");
+        }
+
+        
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
