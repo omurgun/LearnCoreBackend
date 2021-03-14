f1171c3f9ba2624a99c8cea253ce3e3be6ba018e
ömür <omur_gun@outlook.com>
2021-03-14 16:18:13 +0300

    commit

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -66,7 +66,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
